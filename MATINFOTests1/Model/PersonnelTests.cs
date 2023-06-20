b4c1937725574d0ca32461d5ee3dceed5a788ccb
MatteoOVEL <matteo.ovel@etu.univ-savoie.fr>
2023-06-20 20:33:37 +0200

    fdhhfdhdfhdffhdsf

--- MATINFOTests1/Model/PersonnelTests.cs
+++ MATINFOTests1/Model/PersonnelTests.cs
@@ -26,10 +26,8 @@ namespace MATINFO.Tests
         {
             Personnel personnel1 = new Personnel("OVEL", "Mattéo", "matteoovel@gmail.com");
             personnel1.Create();
-
             Personnel personnel2 = new Personnel("OVEL", "Mattéo", "matteoovel@gmail.com");
             personnel2.Read();
-
             Assert.AreEqual(personnel1, personnel2, "Test de creation d'un Personnel");
 
         }
@@ -39,12 +37,9 @@ namespace MATINFO.Tests
         {
             Personnel personnel1 = new Personnel("DE SOUSA", "Hugo", "hugo.desousa@gmail.com");
             personnel1.Create();
-
             personnel1.Delete();
-
             Personnel personnel2 = new Personnel("DE SOUSA", "Hugo", "hugo.desousa@gmail.com");
             personnel2.Read();
-
             Assert.AreEqual(0, personnel2.Id_personnel, "Test de suppression d'un Personnel");
 
         }
@@ -54,10 +49,8 @@ namespace MATINFO.Tests
         {
             Personnel personnel1 = new Personnel("LAVAL", "Quentin", "Quentin.laval@gmail.com");
             personnel1.Create();
-
             Personnel personnel2 = new Personnel("LAVAL", "Quentin", "Quentin.laval@gmail.com");
             personnel2.Read();
-
             Assert.AreEqual(personnel1, personnel2, "Test de lecture d'un Personnel");
 
         }
@@ -67,15 +60,12 @@ namespace MATINFO.Tests
         {
             Personnel personnel1 = new Personnel("DIARD", "Benoît", "benoît.diard@gmail.com");
             personnel1.Create();
-
             personnel1.Prenom = "Nathalie";
             personnel1.Nom = "GRUSON";
             personnel1.Email = "nath.gruson@gmail.com";
             personnel1.Update();
-
             Personnel personnel2 = new Personnel("GRUSON", "Nathalie", "nath.gruson@gmail.com");
             personnel2.Read();
-
             Assert.AreEqual(personnel2, personnel1, "Test de mise à jour d'un Personnel");
 
         }
